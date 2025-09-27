d4e1708d25b5f3f3c96eeb9e311c209740075237
robertclemens <robertclemens@gmail.com>
2025-09-27 11:54:52 -0500

    v1.1.0 release

--- bot.c
+++ bot.c
@@ -7,10 +7,12 @@
 #include <unistd.h>
 
 static volatile bool g_shutdown_flag = false;
+
 void handle_signal(int signum) {
   (void)signum;
   g_shutdown_flag = true;
 }
+
 void setup_signals(void) {
   struct sigaction sa;
   memset(&sa, 0, sizeof(sa));
@@ -21,28 +23,4 @@ void setup_signals(void) {
   sigaction(SIGTERM, &sa, NULL);
   sa.sa_handler = SIG_IGN;
   sigaction(SIGPIPE, &sa, NULL);
-}
-void daemonize(void) {
-  pid_t pid = fork();
-  if (pid < 0) exit(EXIT_FAILURE);
-  if (pid > 0) exit(EXIT_SUCCESS);
-  if (setsid() < 0) exit(EXIT_FAILURE);
-  pid = fork();
-  if (pid < 0) exit(EXIT_FAILURE);
-  if (pid > 0) exit(EXIT_SUCCESS);
-  umask(0);
-  close(STDIN_FILENO);
-  close(STDOUT_FILENO);
-  close(STDERR_FILENO);
-}
-
-void change_proc_name(int argc, char *argv[]) {
-  if (argc == 0 || argv[0] == NULL) return;
-
-  size_t total_len = 0;
-  for (int i = 0; i < argc; i++) {
-    total_len += strlen(argv[i]) + 1;
-  }
-  memset(argv[0], 0, total_len);
-  snprintf(argv[0], total_len, "%s", FAKE_PS);
 }
