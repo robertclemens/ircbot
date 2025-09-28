a7bdce8abd0e6dbcaf309ec9e7f742ee67380d8e
robertclemens <robertclemens@gmail.com>
2025-09-28 11:49:27 -0500

    v1.1.1 - Change AES-256-CBC to AES-256-GCM

--- bot_comms.c
+++ bot_comms.c
@@ -25,9 +25,7 @@ static char *base64_encode(const unsigned char *input, int length) {
 
 void bot_comms_send_command(bot_state_t *state, const char *target_nick,
                             const char *format, ...) {
-  if (!target_nick || !format || state->bot_comm_pass[0] == '\0') {
-    return;
-  }
+  if (!target_nick || !format || state->bot_comm_pass[0] == '\0') return;
 
   char command_part[256];
   va_list args;
@@ -39,50 +37,30 @@ void bot_comms_send_command(bot_state_t *state, const char *target_nick,
   time_t current_time = time(NULL);
   uint64_t nonce;
   RAND_bytes((unsigned char *)&nonce, sizeof(nonce));
-
   snprintf(plaintext_message, sizeof(plaintext_message), "%ld:%llu:%s",
            current_time, (unsigned long long)nonce, command_part);
 
-  unsigned char *encrypted_data = NULL;
-  int encrypted_len = crypto_aes_encrypt_decrypt(
-      true, state->bot_comm_pass, (const unsigned char *)plaintext_message,
-      strlen(plaintext_message), &encrypted_data);
+  unsigned char key[32];
+  EVP_BytesToKey(EVP_aes_256_gcm(), EVP_sha256(), NULL,
+                 (const unsigned char *)state->bot_comm_pass,
+                 strlen(state->bot_comm_pass), 1, key, NULL);
+
+  int plaintext_len = strlen(plaintext_message);
+  unsigned char ciphertext[512 + GCM_IV_LEN];
+  unsigned char tag[GCM_TAG_LEN];
+
+  int encrypted_len =
+      crypto_aes_gcm_encrypt((unsigned char *)plaintext_message, plaintext_len,
+                             key, ciphertext + GCM_IV_LEN, tag);
 
   if (encrypted_len > 0) {
-    char *encoded_data = base64_encode(encrypted_data, encrypted_len);
-    if (encoded_data) {
-      irc_printf(state, "PRIVMSG %s :%s\r\n", target_nick, encoded_data);
-      free(encoded_data);
-    }
-    free(encrypted_data);
-  }
-}
+    char *encoded_ciphertext = base64_encode(ciphertext, encrypted_len);
+    char *encoded_tag = base64_encode(tag, GCM_TAG_LEN);
 
-void bot_comms_send_command_bak(bot_state_t *state, const char *target_nick,
-                                const char *command, const char *channel) {
-  if (!target_nick || !command || !channel || state->bot_comm_pass[0] == '\0') {
-    return;
-  }
+    irc_printf(state, "PRIVMSG %s :%s:%s\r\n", target_nick, encoded_ciphertext,
+               encoded_tag);
 
-  char plaintext_command[256];
-  time_t current_time = time(NULL);
-  uint64_t nonce;
-  RAND_bytes((unsigned char *)&nonce, sizeof(nonce));
-
-  snprintf(plaintext_command, sizeof(plaintext_command), "%ld:%llu:%s %s",
-           current_time, (unsigned long long)nonce, command, channel);
-
-  unsigned char *encrypted_data = NULL;
-  int encrypted_len = crypto_aes_encrypt_decrypt(
-      true, state->bot_comm_pass, (const unsigned char *)plaintext_command,
-      strlen(plaintext_command), &encrypted_data);
-
-  if (encrypted_len > 0) {
-    char *encoded_data = base64_encode(encrypted_data, encrypted_len);
-    if (encoded_data) {
-      irc_printf(state, "PRIVMSG %s :%s\r\n", target_nick, encoded_data);
-      free(encoded_data);
-    }
-    free(encrypted_data);
+    free(encoded_ciphertext);
+    free(encoded_tag);
   }
 }
