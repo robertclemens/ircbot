ed9e20fa0868b26e75cf0b101b9d8bddf94e4d21
robertclemens <robertclemens@gmail.com>
2025-09-28 11:49:34 -0500

    v1.1.1 - Change AES-256-CBC to AES-256-GCM

--- crypto.c
+++ crypto.c
@@ -1,62 +1,63 @@
 #include <openssl/evp.h>
 #include <openssl/rand.h>
 #include <string.h>
+
 #include "bot.h"
 
-int crypto_aes_encrypt_decrypt(bool encrypt, const char *password,
-                               const unsigned char *data, int data_len,
-                               unsigned char **output) {
-  unsigned char key[32], iv[16];
-  unsigned char salt[SALT_SIZE];
+int crypto_aes_gcm_encrypt(const unsigned char *plaintext, int plaintext_len,
+                           const unsigned char *key, unsigned char *ciphertext,
+                           unsigned char *tag) {
+  EVP_CIPHER_CTX *ctx;
+  int len;
+  int ciphertext_len;
+  unsigned char iv[GCM_IV_LEN];
 
-  EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-  if (!ctx) return -1;
+  RAND_bytes(iv, sizeof(iv));
 
-  if (encrypt) {
-    RAND_bytes(salt, sizeof(salt));
-    EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha256(), salt,
-                   (const unsigned char *)password, strlen(password), 1, key,
-                   iv);
-    EVP_CipherInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv, 1);
+  if (!(ctx = EVP_CIPHER_CTX_new())) return -1;
+  if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL))
+    return -1;
+  if (1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, GCM_IV_LEN, NULL))
+    return -1;
+  if (1 != EVP_EncryptInit_ex(ctx, NULL, NULL, key, iv)) return -1;
+  if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+    return -1;
+  ciphertext_len = len;
+  if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) return -1;
+  ciphertext_len += len;
+  if (1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, GCM_TAG_LEN, tag))
+    return -1;
 
-    int output_len = data_len + 16 + SALT_SIZE;
-    *output = malloc(output_len);
-    if (!*output) {
-      EVP_CIPHER_CTX_free(ctx);
-      return -1;
-    }
+  memcpy(ciphertext - GCM_IV_LEN, iv, GCM_IV_LEN);
 
-    memcpy(*output, salt, SALT_SIZE);
+  EVP_CIPHER_CTX_free(ctx);
+  return ciphertext_len + GCM_IV_LEN;
+}
 
-    int len1 = 0, len2 = 0;
-    EVP_CipherUpdate(ctx, (*output) + SALT_SIZE, &len1, data, data_len);
-    EVP_CipherFinal_ex(ctx, (*output) + SALT_SIZE + len1, &len2);
+int crypto_aes_gcm_decrypt(const unsigned char *ciphertext, int ciphertext_len,
+                           const unsigned char *key, unsigned char *plaintext,
+                           unsigned char *tag) {
+  EVP_CIPHER_CTX *ctx;
+  int len;
+  int plaintext_len;
+  unsigned char iv[GCM_IV_LEN];
 
-    EVP_CIPHER_CTX_free(ctx);
-    return SALT_SIZE + len1 + len2;
+  memcpy(iv, ciphertext, GCM_IV_LEN);
 
-  } else {
-    memcpy(salt, data, SALT_SIZE);
-    EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha256(), salt,
-                   (const unsigned char *)password, strlen(password), 1, key,
-                   iv);
-    EVP_CipherInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv, 0);
+  if (!(ctx = EVP_CIPHER_CTX_new())) return -1;
+  if (!EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL)) return -1;
+  if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, GCM_IV_LEN, NULL))
+    return -1;
+  if (!EVP_DecryptInit_ex(ctx, NULL, NULL, key, iv)) return -1;
+  if (!EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext + GCM_IV_LEN,
+                         ciphertext_len - GCM_IV_LEN))
+    return -1;
+  plaintext_len = len;
+  if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, GCM_TAG_LEN, tag))
+    return -1;
+  if (1 > EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) return -1;
+  plaintext_len += len;
 
-    const unsigned char *ciphertext = data + SALT_SIZE;
-    int ciphertext_len = data_len - SALT_SIZE;
-
-    int output_len = ciphertext_len + 16;
-    *output = malloc(output_len);
-    if (!*output) {
-      EVP_CIPHER_CTX_free(ctx);
-      return -1;
-    }
-
-    int len1 = 0, len2 = 0;
-    EVP_DecryptUpdate(ctx, *output, &len1, ciphertext, ciphertext_len);
-    EVP_DecryptFinal_ex(ctx, (*output) + len1, &len2);
-
-    EVP_CIPHER_CTX_free(ctx);
-    return len1 + len2;
-  }
+  EVP_CIPHER_CTX_free(ctx);
+  return plaintext_len;
 }
